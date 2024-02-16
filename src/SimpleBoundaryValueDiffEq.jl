804cb72d4bfd55842ca16332357d0b7a4fdfeb42
Avik Pal <avikpal@mit.edu>
2024-02-15 20:49:08 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.48

---
+++ src/SimpleBoundaryValueDiffEq.jl
@@ -0,0 +1,5 @@
+module SimpleBoundaryValueDiffEq
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,14 @@
+using SimpleBoundaryValueDiffEq
+using Test
+using Aqua
+using JET
+
+@testset "SimpleBoundaryValueDiffEq.jl" begin
+    @testset "Code quality (Aqua.jl)" begin
+        Aqua.test_all(SimpleBoundaryValueDiffEq)
+    end
+    @testset "Code linting (JET.jl)" begin
+        JET.test_package(SimpleBoundaryValueDiffEq; target_defined_modules = true)
+    end
+    # Write your tests here.
+end
